230ddca8c8add890ee1819806d80ffa5766c1490
hsinpa <hsinpa@gmail.com>
2020-08-12 21:17:30 +0800

    Add preparation code on mesh generation

--- AROrigamiGroceryUnity/Assets/Script/Mesh/MeshGenerator.cs
+++ AROrigamiGroceryUnity/Assets/Script/Mesh/MeshGenerator.cs
@@ -7,36 +7,48 @@ public class MeshGenerator : MonoBehaviour
 
     public SquareGrid squareGrid;
 
-    public void GenerateMesh(Color[,] map, float squareSize) {
-        squareGrid = new SquareGrid(map, squareSize);
+    public void GenerateMesh(Color[] map, int width, int height, float squareSize) {
+        squareGrid = new SquareGrid(map, width, height, squareSize);
     }
 
     public class SquareGrid {
         public Square[,] sqaures;
 
-        public SquareGrid(Color[,] map, float squareSize) {
-            int nodeCountX = map.GetLength(0);
-            int nodeCountY = map.GetLength(1);
+        public SquareGrid(Color[] map, int width, int height,  float squareSize) {
+            int nodeCountX = width;
+            int nodeCountY = height;
             float mapWidth = nodeCountX * squareSize;
             float mapHeight = nodeCountY * squareSize;
 
+            int totalLen = map.Length;
+
             ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
 
             for (int x = 0; x < nodeCountX; x++) {
                 for (int y = 0; y < nodeCountY; y++)
                 {
+
+                    int mapIndex = (nodeCountY * y) + x;
+
+
                     Vector3 pos = new Vector3(-(mapWidth / 2f) + x * squareSize + (squareSize / 2f), 0, -(mapHeight / 2f) + y * squareSize + (squareSize / 2));
-                    controlNodes[x, y] = new ControlNode(pos, map[x, y].r == 1, squareSize);
+                    controlNodes[x, y] = new ControlNode(pos, map[mapIndex].r <= 0.1f, squareSize);
                 }
             }
 
-            this.sqaures = new Square[nodeCountX -1, nodeCountY -1];
-            for (int x = 0; x < nodeCountX; x++)
+            sqaures = new Square[nodeCountX -1, nodeCountY -1];
+            for (int x = 0; x < nodeCountX - 1; x++)
             {
-                for (int y = 0; y < nodeCountY; y++)
+                for (int y = 0; y < nodeCountY - 1; y++)
                 {
                     //TopLeft, TopRight, BottomRight, BottomLeft
-                    this.sqaures[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                    var TopLeft = controlNodes[x, y + 1];
+                    var TopRight = controlNodes[x + 1, y + 1];
+                    var BottomRight = controlNodes[x + 1, y];
+                    var BottomLeft = controlNodes[x, y];
+
+                    sqaures[x, y] = new Square(TopLeft, TopRight, BottomRight, BottomLeft);
                 }
             }
         }
--- AROrigamiGroceryUnity/Assets/Script/Mesh/TextureMeshPreview.cs
+++ AROrigamiGroceryUnity/Assets/Script/Mesh/TextureMeshPreview.cs
@@ -15,10 +15,43 @@ public class TextureMeshPreview : MonoBehaviour
 
         var colors = ReadPixel();
         Debug.Log(colors.Length);
+
+        meshGenerator.GenerateMesh(colors, maskTexture.width, maskTexture.height, 1);
     }
 
     private Color[] ReadPixel()
     {
         return maskTexture.GetPixels(0, 0, maskTexture.width, maskTexture.height);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (meshGenerator != null && meshGenerator.squareGrid != null && meshGenerator.squareGrid.sqaures != null) {
+
+            var square = meshGenerator.squareGrid.sqaures;
+            for (int x = 0; x < square.GetLength(0); x++)
+            {
+                for (int y = 0; y < square.GetLength(1); y++)
+                {
+                    Gizmos.color = (square[x, y].topLeft.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(square[x, y].topLeft.position, Vector3.one * 0.4f);
+
+                    Gizmos.color = (square[x, y].topRight.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(square[x, y].topRight.position, Vector3.one * 0.4f);
+
+                    Gizmos.color = (square[x, y].bottomRight.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(square[x, y].bottomRight.position, Vector3.one * 0.4f);
+
+                    Gizmos.color = (square[x, y].bottomLeft.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(square[x, y].bottomLeft.position, Vector3.one * 0.4f);
+
+                    Gizmos.color = Color.gray;
+                    Gizmos.DrawCube(square[x, y].centerTop.position, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(square[x, y].centerRight.position, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(square[x, y].centerLeft.position, Vector3.one * 0.15f);
+                    Gizmos.DrawCube(square[x, y].centerBottom.position, Vector3.one * 0.15f);
+                }
+            }
+        }   
     }
 }
