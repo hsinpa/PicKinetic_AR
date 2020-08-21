609213a4174ed7c3be9be976c9ac9a90931188b6
hsinpa <hsinpa@gmail.com>
2020-08-21 18:10:36 +0800

    Add guassian blur

---
+++ AROrigamiGroceryUnity/Assets/Script/Algorithm/Edge/EdgeImageDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeImageDetector : MonoBehaviour
+{
+
+    [SerializeField]
+    private Material EdgeMaterial;
+
+
+    [SerializeField]
+    private Texture2D inputImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlueImage(inputImage);    
+    }
+
+    void BlueImage(Texture2D input) {
+        Texture aTexture;
+        RenderTexture rTex;
+
+
+    }
+
+
+
+}
