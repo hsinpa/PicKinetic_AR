1020d2bae37f716313489f18b7aaed3f791aeaeb
hsinpa <hsinpa@gmail.com>
2020-08-25 17:16:14 +0800

    Prepare to test offline texture scanning

---
+++ AROrigamiGroceryUnity/Assets/Script/Camera/OffCameraSetting.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffCameraSetting : MonoBehaviour
+{
+    public RawImage preview;
+    public Material rotateMat;
+
+    public Texture2D inputTex;
+    private Texture2D cropTex;
+    private Texture2D previewTex;
+    private RenderTexture previewRenderer;
+
+    TextureUtility TextureUtility;
+
+    private const float degreeToRadian = Mathf.PI / 180;
+    private Rect rectReadPicture;
+    int textureSize = 256;
+
+    private void Start()
+    {
+        TextureUtility = new TextureUtility();
+        PrepareTexture();
+
+        preview.texture = RotateAndScaleImage(GrabTextureRadius(), 0);
+    }
+
+    private void PrepareTexture()
+    {
+        previewRenderer = TextureUtility.GetRenderTexture(textureSize);
+        previewTex = new Texture2D(textureSize, textureSize);
+        rectReadPicture = new Rect(0, 0, textureSize, textureSize);
+
+
+    }
+
+    private Texture2D GrabTextureRadius()
+    {
+
+        var texInfo = TextureUtility.GrabTextureRadius(inputTex.width, inputTex.height, 0.6f);
+
+        if (cropTex == null)
+            cropTex = new Texture2D(texInfo.width, texInfo.height);
+
+        cropTex.SetPixels(inputTex.GetPixels(texInfo.x, texInfo.y, texInfo.width, texInfo.height));
+        cropTex.Apply();
+
+        return cropTex;
+    }
+
+    private Texture2D RotateAndScaleImage(Texture p_texture, int degree)
+    {
+        float radian = degreeToRadian * degree;
+
+        rotateMat.SetFloat("_Rotation", radian);
+
+        Graphics.Blit(p_texture, previewRenderer, rotateMat, 0);
+
+        RenderTexture.active = previewRenderer;
+        // Read pixels
+        previewTex.ReadPixels(rectReadPicture, 0, 0);
+        previewTex.Apply();
+        RenderTexture.active = null;
+
+        return previewTex;
+    }
+}
