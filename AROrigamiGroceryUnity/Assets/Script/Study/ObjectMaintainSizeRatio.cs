42f6870e1a36711373c61d6293c209f0eb2bb577
hsinpa <hsinpa@gmail.com>
2020-09-20 23:27:06 +0800

    Maintain scale ratio according to position test

---
+++ AROrigamiGroceryUnity/Assets/Script/Study/ObjectMaintainSizeRatio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMaintainSizeRatio : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField, Range(0, 20)]
+    private float _SizeStrength = 1;
+
+    [SerializeField, Range(-2, 2)]
+    private float _HeightStrength = 1;
+
+    [SerializeField]
+    private Collider _ObjectCollider;
+
+    private Vector3 _OriginalPos;    
+
+    private void Start()
+    {
+        _OriginalPos = transform.position;
+        Debug.Log(_ObjectCollider.bounds.size);
+    }
+
+    private void Update()
+    {
+        float size = _ObjectCollider.bounds.size.y;
+        float sizeMagnitue = (_camera.transform.position - transform.position).magnitude * _SizeStrength;
+        transform.localScale = new Vector3(sizeMagnitue, sizeMagnitue, sizeMagnitue);
+    }
+
+
+}
