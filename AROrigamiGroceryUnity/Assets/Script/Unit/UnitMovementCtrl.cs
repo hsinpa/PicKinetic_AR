91c2f4db71c7a26684412148f05da648954b6e0b
hsinpa <hsinpa@gmail.com>
2020-10-26 00:11:12 +0800

    Add movement

--- AROrigamiGroceryUnity/Assets/Script/Unit/UnitMovementCtrl.cs
+++ AROrigamiGroceryUnity/Assets/Script/Unit/UnitMovementCtrl.cs
@@ -7,33 +7,124 @@ namespace AROrigami
 {
     public class UnitMovementCtrl : MonoBehaviour
     {
+        [SerializeField]
+        private float move_speed = 1;
+
         //Only detect plane collider
         private static RaycastHit[] m_Results = new RaycastHit[1];
 
         private float _sPerlinX, _sPerlinY;
         private float raycastLength = 10;
+        private Vector3 moveDirection = Vector3.zero;
+
+        private enum State { 
+            Idle, Rotate, Move
+        }
+
+        private State state;
+
+        private void Start()
+        {
+            SetUp();
+        }
+
 
         private void SetUp()
         {
+            state = State.Rotate;
             int minP = 0, maxP = 30000;
             _sPerlinX = UtilityMethod.GetRandomNumber(minP, maxP);
             _sPerlinY = UtilityMethod.GetRandomNumber(minP, maxP);
+            ChangeForwardDir();
         }
 
         private void Update() {
-            DetectIsFloorFront();
+            _sPerlinX += 0.001f;
+            _sPerlinY += 0.001f;
+            ProcessBahavior();
+        }
+
+        private void ProcessBahavior() {
+            switch (this.state) {
+                case State.Idle:
+                    Idle();
+                break;
+
+                case State.Rotate:
+                    MoveRotate();
+                break;
+
+                case State.Move:
+                    ObjectMove();
+                break;
+            }
+        }
+
+        private void Idle() { 
+            
+        }
+
+        private void MoveRotate() {
+            Vector3 currentFace = transform.rotation.eulerAngles;
+            float faceAngle = GetAngle(currentFace.y);
+
+            float diff = Mathf.Abs( GetAngle(moveDirection.y) - faceAngle);
+
+
+            if (diff < 1f)
+            {
+                state = State.Move; 
+            }
+            else {
+                Vector3 miniRotate = Vector3.Lerp(currentFace, moveDirection, 0.1f);
+
+                transform.rotation = Quaternion.Euler(miniRotate);
+            }
+        }
+
+        private void ChangeForwardDir() {
+
+            float randomNum = UtilityMethod.GetRandomNumber(-45, 45);
+
+            randomNum += (moveDirection.y - 180);
+
+            moveDirection.Set(0, Mathf.Abs(GetAngle(randomNum)), 0);
+        }
+
+        private void ObjectMove() {
+
+            bool canWalk = DetectIsFloorFront();
+
+            if (canWalk)
+            {
+                float yDir = ((Mathf.PerlinNoise(_sPerlinX, _sPerlinY) * 2) - 1) * 360;
+                //yDir = Mathf.Lerp(moveDirection.y, yDir, 0.1f) * 0.001f;
+
+                moveDirection.Set(0, GetAngle(moveDirection.y + (yDir * 0.0035f)), 0);
+
+                transform.rotation = Quaternion.Euler(moveDirection);
+                transform.position = transform.position + (transform.forward * Time.deltaTime * move_speed);
+            }
+            else {
+                ChangeForwardDir();
+                state = State.Rotate;
+            }
+        }
+
+        private float GetAngle(float angle) {
+            return angle % 360f;
         }
 
         private bool DetectIsFloorFront() {
 
-            Vector3 direciton = transform.forward + new Vector3(0, -0.6f, 0);
+            Vector3 direciton = (transform.forward) + new Vector3(0, -0.6f, 0);
 
             int hits = Physics.RaycastNonAlloc(transform.position, direciton, m_Results, raycastLength, ParameterFlag.ColliderLayer.FloorLayer);
 
             //Debug.Log("Forward "+ direciton + ", hits " + hits);
 
             Debug.DrawRay(transform.position, direciton, Color.blue);
-            return true;
+            return hits > 0;
         }
     }
 }
